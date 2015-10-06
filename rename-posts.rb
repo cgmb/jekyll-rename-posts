d52127c40291a2c31b7c2dc6b3071c74d48e5d71
Cory Bloor <cgbloor@ucalgary.ca>
2015-10-06 16:40:18 -0600

    Fix broken symlinks when old_slug contains dirs
    Without this patch, the symlink is created at the old_slug location
    with the target being the new slug's relative path from the jekyll
    root directory. Of course, that only works if the old_slug was in the
    jekyll root directory.
    
    This fix finds the relative path from the old_slug to the new slug,
    and specifies that path as the link target.

--- rename-posts.rb
+++ rename-posts.rb
@@ -1,3 +1,5 @@
+require 'pathname'
+
 module Jekyll
   class Post
     def write_and_symlink(dest)
@@ -10,10 +12,13 @@ module Jekyll
       if not self.data['old-slug'].nil?
         path = File.join(CGI.unescape(self.url)).gsub(/^\//, '')
         path = File.join(path, "index.html") if template[/\.html$/].nil?
+        path = File.join(site.dest, path)
         old_slug = File.join(site.dest, self.data['old-slug'])
+        old_slug_dir = File.dirname(old_slug)
+        path = (Pathname.new path).relative_path_from(Pathname.new old_slug_dir)
 
         # We write the symlink directly on the site destination
-        FileUtils.mkdir_p(File.dirname(old_slug))
+        FileUtils.mkdir_p(old_slug_dir)
         File.symlink(path, old_slug)
       end
     end
